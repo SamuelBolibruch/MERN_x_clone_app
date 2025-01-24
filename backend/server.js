e4a5694aabf5cf326fafd1dd6db31dcf1143e0ea
Samuel Bolibruch <samo.bolibruch@gmail.com>
2025-01-24 18:52:29 +0100

    Auth Routes and Controller

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/app.routes.js";
 import connectMongoDB from "./db/connectMongoDb.js";
@@ -9,6 +10,10 @@ console.log("MONGO_URI:", process.env.MONGO_URI);
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
